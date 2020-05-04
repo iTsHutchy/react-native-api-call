9b078e0b220268017510f56f09ca247b6a44d4d6
Michael Hutchinson <michaelhutchinson@michaels-mbp.mynet>
2020-05-04 15:34:41 +0100

    Introduce API
    - Added api call into app
    - Start writing code to call the api (WIP) and display the results

--- App.js
+++ App.js
@@ -1,12 +1,50 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true,
+      dateSource: null,
+    }
+  }
+
+  componentDidMount() {
+    return fetch('https://reactnative.dev/movies.json')
+    .then((response) => response.json())
+    .then((responseJson) => {
+      this.setState({
+        isLoading: false,
+        dateSource: responseJson.movies,
+      })
+    })
+    .catch((error) => {
+      console.log(error)
+    });
+  }
+
+  render() {
+    if (this.state.isLoading) {
+      return (
+        <View style={styles.container}>
+          <ActivityIndicator />
+        </View>
+      )
+    } else {
+      let movies = this.state.dataSource.map((val, key) => {
+        return <View key={key} style={styles.item}>
+          <Text>{val.title}</Text>
+        </View>
+      });
+      return (
+        <View style={styles.container}>
+          {movies}
+        </View>
+      );
+    }
+  }
 }
 
 const styles = StyleSheet.create({
